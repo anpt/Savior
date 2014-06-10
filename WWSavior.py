1196c866fefc8a69a1be5b6e60ff1e3ff05fda4c
Anton <anton.l.petersson@gmail.com>
2014-06-10 15:35:27 +0200

    Help flags added

--- WWSavior.py
+++ WWSavior.py
@@ -1,73 +1,72 @@
-#WWSavior is a program to create template in a wonderware system.
-#First argument point to a mall how the template should be created, it can contains several templates.
-#Mark the points that gonna be replaced with {0}, {1} and so on.
-#Second argument point to the csv
-#Rest of the arguments check with parmeter to replace {i} with.
+#Copyright 2014 Anton Petersson
+
+#This program is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation, either version 3 of the License, or
+#(at your option) any later version.
+
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+
+#You should have received a copy of the GNU General Public License
+#along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#-------------------------------------------------------------------#
+
+#Savior is a program to create template to a system.
+
 
 import sys
 import csv
+import argparse
 
-srcFile = open(sys.argv[1], 'r')
-keyFile = open(sys.argv[2], 'r')
-output = open('output','w')
+#Commandline funcionality
+parser = argparse.ArgumentParser()
+parser.add_argument("--pattern", help="File that contians the pattern for the template")
+parser.add_argument("--file", help="csv file that contains data for insert in the pattern")
+parser.add_argument("--par", type=int, nargs='+', help="list of parmater how to map pattern with the data in file, first column ")
+parser.add_argument("--out", help="File to write the result to")
+args = parser.parse_args();
+#--------------------------#
 
-<<<<<<< HEAD
+srcFile = open(args.pattern, 'r')
+keyFile = open(args.file, 'r')
+output = open(args.out,'w')
+
 #var for dubblets
 oldRow = []
 first = True
-=======
-#Varibles for dublett
-oldRow = []
-oldTmp = []
-first = True;
->>>>>>> checkforemptylines
+
 
 template=srcFile.read()
 keys = csv.reader(keyFile)
 jump = False
 
 for row in keys:        
-    index = 3
+    index = 0
     tmp = template
     result = []
     oldTmp = []
-    while index < len(sys.argv):
+    while index < len(args.par):
         try:
-            if (row[int(sys.argv[index])] == ""):
+            if (row[int(args.par[index])] == ""):
                 jump = True
                 break
-            if (first == False):
-                if (row[int(sys.argv[index])] == oldRow[index-2]):
-                    jump = True
-                    break
         except:
             jump = True
             break        
-        result.append(row[int(sys.argv[index])])
-<<<<<<< HEAD
+        result.append(row[int(args.par[index])])
         index = index +1
-    if (oldRow!=result or first):
-=======
-        oldTmp.append(row[int(sys.argv[index])])
-        index = index +1
-        
-        
->>>>>>> checkforemptylines
+    if (oldRow!=result or first):        
         if (jump == False):
             tmp = tmp.format(*result)
             print(tmp,file=output),
         else:
-<<<<<<< HEAD
             print("Not a Value"),
     else:
-        print("duplicate Row! Aborted row insert"),
+        print("duplicate Row! Aborted row insert. cont"),
     oldRow = result
     first = False
-=======
-            print("Inga giltiga värden"),
->>>>>>> checkforemptylines
     jump = False
-    first = False
-    oldRow = oldTmp
-    
-    
